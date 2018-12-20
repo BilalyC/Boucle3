1e13f0aaf2dbfe69611e804f4ef62769be087714
BilalyC <bilaly.camara@outlook.com>
2018-12-20 13:21:05 +0100

    Exercice 3 Complet

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter;
+            for (counter = 0; counter < 10; counter++) //Variable, Condition, Itération
+            {
+                Console.WriteLine("Bonjour, je suis un message généré à l'aide d'une boucle.");
+            }
+        }
+    }
+}
